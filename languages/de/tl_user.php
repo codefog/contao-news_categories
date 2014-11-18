6ce5ee6539b5dfbd71d3a99613e94e70154566f4
Frank Hoppe <hoppe.berlin@googlemail.com>
2014-11-18 09:53:52 +0100

    Create tl_user.php

---
+++ languages/de/tl_user.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * news_categories extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2011-2014 Codefog
+ *
+ * @package news_categories
+ * @author  Webcontext <http://webcontext.com>
+ * @author  Codefog <info@codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * @license LGPL
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_user']['newscategories']         = array('Nachrichten-Kategorien verwalten', 'Benutzer erlauben, Kategorien zu verwalten');
+$GLOBALS['TL_LANG']['tl_user']['newscategories_default'] = array('Standard-Nachrichten-Kategorien', 'Hier können die Standard-Nachrichten-Kategorien gesetzt werden, die verwendet werden, wenn der Benutzer keine Berechtigung hat um Kategorien zu bearbeiten.');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_user']['newscategories_legend'] = 'Berechtigungen Nachrichten-Kategorien';
