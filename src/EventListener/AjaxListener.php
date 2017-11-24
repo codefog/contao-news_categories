e3ce64d379eeaf5ac80b531b4c9d0c2b3a860abb
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2017-11-24 17:21:42 +0100

    Improved the ajax listener

--- src/EventListener/AjaxListener.php
+++ src/EventListener/AjaxListener.php
@@ -4,16 +4,38 @@ namespace Codefog\NewsCategoriesBundle\EventListener;
 
 use Codefog\NewsCategoriesBundle\Widget\NewsCategoriesPickerWidget;
 use Contao\CoreBundle\Exception\ResponseException;
+use Contao\CoreBundle\Framework\FrameworkAwareInterface;
+use Contao\CoreBundle\Framework\FrameworkAwareTrait;
+use Contao\CoreBundle\Monolog\ContaoContext;
 use Contao\Database;
 use Contao\DataContainer;
 use Contao\Input;
 use Contao\StringUtil;
 use Contao\System;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
-class AjaxListener
+class AjaxListener implements FrameworkAwareInterface
 {
+    use FrameworkAwareTrait;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * AjaxListener constructor.
+     *
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * On execute the post actions
      *
@@ -34,20 +56,32 @@ class AjaxListener
      */
     private function reloadNewsCategoriesWidget(DataContainer $dc)
     {
-        $intId = Input::get('id');
-        $strField = $dc->inputName = Input::post('name');
+        /**
+         * @var Database $db
+         * @var Input $input
+         */
+        $db = $this->framework->createInstance(Database::class);
+        $input = $this->framework->getAdapter(Input::class);
+
+        $id = $input->get('id');
+        $field = $dc->inputName = $input->post('name');
 
         // Handle the keys in "edit multiple" mode
-        if (Input::get('act') === 'editAll') {
-            $intId = preg_replace('/.*_([0-9a-zA-Z]+)$/', '$1', $strField);
-            $strField = preg_replace('/(.*)_[0-9a-zA-Z]+$/', '$1', $strField);
+        if ($input->get('act') === 'editAll') {
+            $id = preg_replace('/.*_([0-9a-zA-Z]+)$/', '$1', $field);
+            $field = preg_replace('/(.*)_[0-9a-zA-Z]+$/', '$1', $field);
         }
 
-        $dc->field = $strField;
+        $dc->field = $field;
 
         // The field does not exist
-        if (!isset($GLOBALS['TL_DCA'][$dc->table]['fields'][$strField])) {
-            System::log('Field "' . $strField . '" does not exist in DCA "' . $dc->table . '"', __METHOD__, TL_ERROR);
+        if (!isset($GLOBALS['TL_DCA'][$dc->table]['fields'][$field])) {
+            $this->logger->log(
+                LogLevel::ERROR,
+                sprintf('Field "%s" does not exist in DCA "%s"', $field, $dc->table),
+                ['contao' => new ContaoContext(__METHOD__, TL_ERROR)]
+            );
+
             throw new BadRequestHttpException('Bad request');
         }
 
@@ -55,25 +89,32 @@ class AjaxListener
         $value = null;
 
         // Load the value
-        if (Input::get('act') !== 'overrideAll' && $intId > 0 && Database::getInstance()->tableExists($dc->table)) {
-            $row = Database::getInstance()->prepare("SELECT * FROM " . $dc->table . " WHERE id=?")
-                ->execute($intId);
+        if ($input->get('act') !== 'overrideAll' && $id > 0 && $db->tableExists($dc->table)) {
+            $row = $db->prepare("SELECT * FROM " . $dc->table . " WHERE id=?")->execute($id);
 
             // The record does not exist
             if ($row->numRows < 1) {
-                System::log('A record with the ID "' . $intId . '" does not exist in table "' . $dc->table . '"', __METHOD__, TL_ERROR);
+                $this->logger->log(
+                    LogLevel::ERROR,
+                    sprintf('A record with the ID "%s" does not exist in table "%s"', $id, $dc->table),
+                    ['contao' => new ContaoContext(__METHOD__, TL_ERROR)]
+                );
+
                 throw new BadRequestHttpException('Bad request');
             }
 
-            $value = $row->$strField;
+            $value = $row->$field;
             $dc->activeRecord = $row;
         }
 
         // Call the load_callback
-        if (is_array($GLOBALS['TL_DCA'][$dc->table]['fields'][$strField]['load_callback'])) {
-            foreach ($GLOBALS['TL_DCA'][$dc->table]['fields'][$strField]['load_callback'] as $callback) {
+        if (is_array($GLOBALS['TL_DCA'][$dc->table]['fields'][$field]['load_callback'])) {
+            /** @var System $systemAdapter */
+            $systemAdapter = $this->framework->getAdapter(System::class);
+
+            foreach ($GLOBALS['TL_DCA'][$dc->table]['fields'][$field]['load_callback'] as $callback) {
                 if (is_array($callback)) {
-                    $value = System::importStatic($callback[0])->{$callback[1]}($value, $dc);
+                    $value = $systemAdapter->importStatic($callback[0])->{$callback[1]}($value, $dc);
                 } elseif (is_callable($callback)) {
                     $value = $callback($value, $dc);
                 }
@@ -81,11 +122,13 @@ class AjaxListener
         }
 
         // Set the new value
-        $value = Input::post('value', true);
+        $value = $input->post('value', true);
 
         // Convert the selected values
         if ($value) {
-            $value = StringUtil::trimsplit("\t", $value);
+            /** @var StringUtil $stringUtilAdapter */
+            $stringUtilAdapter = $this->framework->getAdapter(StringUtil::class);
+            $value = $stringUtilAdapter->trimsplit("\t", $value);
             $value = serialize($value);
         }
 
@@ -93,7 +136,7 @@ class AjaxListener
         $strClass = $GLOBALS['BE_FFL']['newsCategoriesPicker'];
 
         /** @var NewsCategoriesPickerWidget $objWidget */
-        $objWidget = new $strClass($strClass::getAttributesFromDca($GLOBALS['TL_DCA'][$dc->table]['fields'][$strField], $dc->inputName, $value, $strField, $dc->table, $dc));
+        $objWidget = new $strClass($strClass::getAttributesFromDca($GLOBALS['TL_DCA'][$dc->table]['fields'][$field], $dc->inputName, $value, $field, $dc->table, $dc));
 
         throw new ResponseException(new Response($objWidget->generate()));
     }
