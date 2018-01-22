5e797f06db93433a7d759f5f3c3c7fa10073fb11
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2018-01-22 18:18:25 +0100

    Fix display all news if the category was not found (#133)

--- models/NewsModel.php
+++ models/NewsModel.php
@@ -96,7 +96,7 @@ class NewsModel extends \Contao\NewsModel
             $objCategory = $strClass::findPublishedByIdOrAlias(\Input::get($strParam));
 
             if ($objCategory === null) {
-                return null;
+                return ["$t.id=0"];
             }
 
             $arrCategories = static::getCategoriesCache();
