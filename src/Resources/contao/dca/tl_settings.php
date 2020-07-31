fcc550c592c8cef3a911a5dc164f6e8535ae4467
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2020-07-31 09:40:42 +0100

    use contao.slug if available

---
+++ src/Resources/contao/dca/tl_settings.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * News Categories bundle for Contao Open Source CMS.
+ *
+ * @copyright  Copyright (c) 2020, Codefog
+ * @author     Codefog <https://codefog.pl>
+ * @license    MIT
+ */
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['news_categorySlugSetting'] = [
+    'inputType' => 'select',
+    'options_callback' => static function () {
+        return Contao\System::getContainer()->get('contao.slug.valid_characters')->getOptions();
+    },
+    'eval' => ['tl_class' => 'w50', 'includeBlankOption' => true,  'decodeEntities' => true],
+];
+
+PaletteManipulator::create()
+    ->addLegend('news_categories_legend', null, PaletteManipulator::POSITION_AFTER, true)
+    ->addField('news_categorySlugSetting', 'news_categories_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_settings')
+;
