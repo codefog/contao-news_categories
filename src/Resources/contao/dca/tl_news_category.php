6f3af928ac32f7db07d4106c40ab3f1f22809523
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-05-31 18:32:12 +0200

    Fix PHP 8 warnings

--- src/Resources/contao/dca/tl_news_category.php
+++ src/Resources/contao/dca/tl_news_category.php
@@ -93,7 +93,7 @@ $GLOBALS['TL_DCA']['tl_news_category'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_news_category']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '').'\'))return false;Backend.getScrollOffset()"',
             ],
             'show' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_news_category']['show'],
