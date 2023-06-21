e2781097d3ef2302356aa6a20814b3866a268f0c
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-06-21 18:13:06 +0200

    The Contao slug class is always available

--- contao/dca/tl_settings.php
+++ contao/dca/tl_settings.php
@@ -9,18 +9,15 @@
  */
 
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
-use Contao\CoreBundle\Slug\Slug;
 
-if (\class_exists(Slug::class)) {
-    $GLOBALS['TL_DCA']['tl_settings']['fields']['news_categorySlugSetting'] = [
-        'inputType' => 'select',
-        'options_callback' => ['codefog_news_categories.listener.data_container.settings', 'onSlugSettingOptionsCallback'],
-        'eval' => ['tl_class' => 'w50', 'includeBlankOption' => true,  'decodeEntities' => true],
-    ];
+$GLOBALS['TL_DCA']['tl_settings']['fields']['news_categorySlugSetting'] = [
+    'inputType' => 'select',
+    'options_callback' => ['codefog_news_categories.listener.data_container.settings', 'onSlugSettingOptionsCallback'],
+    'eval' => ['tl_class' => 'w50', 'includeBlankOption' => true, 'decodeEntities' => true],
+];
 
-    PaletteManipulator::create()
-        ->addLegend('news_categories_legend', null, PaletteManipulator::POSITION_AFTER, true)
-        ->addField('news_categorySlugSetting', 'news_categories_legend', PaletteManipulator::POSITION_APPEND)
-        ->applyToPalette('default', 'tl_settings')
-    ;
-}
+PaletteManipulator::create()
+    ->addLegend('news_categories_legend', null, PaletteManipulator::POSITION_AFTER, true)
+    ->addField('news_categorySlugSetting', 'news_categories_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_settings')
+;
