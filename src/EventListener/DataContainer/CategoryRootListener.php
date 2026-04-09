bb4f9fc955d8dd8cb803727a61806507d8f1c10f
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-04-09 14:53:31 -0400

    Update CategoryRootListener.php

--- src/EventListener/DataContainer/CategoryRootListener.php
+++ src/EventListener/DataContainer/CategoryRootListener.php
@@ -47,7 +47,7 @@ class CategoryRootListener implements ResetInterface
         if (!isset($this->rootNodesCache[$id])) {
             $archive = $this->connection->fetchAssociative('SELECT limitCategories, categories FROM tl_news_archive WHERE id=?', [$id]);
 
-            if (!$archive['limitCategories']) {
+            if (!$archive || !$archive['limitCategories']) {
                 $this->rootNodesCache[$id] = false;
             } else {
                 $this->rootNodesCache[$id] = StringUtil::deserialize($archive['categories']);
